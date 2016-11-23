876acf0d481c806d9d75566212125211cffbfde4
Stephen Lead <stephen.lead@gmail.com>
2016-11-23 16:37:46 +1100

    update main presentation and hover demo

--- examples/flHover/code.js
+++ examples/flHover/code.js
@@ -22,19 +22,21 @@ require([
 
   esriConfig.defaults.io.corsDetection = false;
 
+  // initialise the map with a basemap, centroid and zoom level
   map = new Map("mapDiv", {
     basemap: "streets",
     center: [-80.94, 33.646],
-    zoom: 8,
-    slider: false
+    zoom: 8
   });
 
+  // define a feature layer, with a definition query to keep it simple
   var countyLayer = new FeatureLayer("http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Demographics/ESRI_Census_USA/MapServer/3", {
     mode: FeatureLayer.MODE_SNAPSHOT,
     outFields: ["NAME", "POP2000"]
   });
   countyLayer.setDefinitionExpression("STATE_NAME = 'South Carolina'");
   
+  // override the layer's symbology using a symbol and renderer
   var symbol = new SimpleFillSymbol(
     SimpleFillSymbol.STYLE_SOLID,
     new SimpleLineSymbol(
@@ -45,8 +47,8 @@ require([
     new Color([125,125,125,0.35])
   );
   countyLayer.setRenderer(new SimpleRenderer(symbol));
-  map.addLayer(countyLayer);
 
+  // define a symbol to use when highlighting the features
   var highlightSymbol = new SimpleFillSymbol(
     SimpleFillSymbol.STYLE_SOLID,
     new SimpleLineSymbol(
@@ -56,22 +58,29 @@ require([
     new Color([125,125,125,0.35])
   );
 
-  //close the dialog when the mouse leaves the highlight graphic
   map.on("load", function(){
+
+    // add the layer to the map
+    map.addLayer(countyLayer);
+
+    // listen for the onMouseOver event on the counties layer
+    // create a new graphic with the geometry from the event.graphic and add it to the maps graphics layer
+    countyLayer.on("mouse-over", function(evt){
+      var content = evt.graphic.attributes["NAME"] + ": " + evt.graphic.attributes["POP2000"];
+      console.log(content);
+
+      var highlightGraphic = new Graphic(evt.graphic.geometry,highlightSymbol);
+      map.graphics.add(highlightGraphic);
+    });
+
+    // Remove the highlight when the mouse leaves the highlight graphic. This listener demonstrates
+    // an event on the map's graphics layer
     map.graphics.on("mouse-out", removeHighlight);
   });
 
-  //listen for when the onMouseOver event fires on the countiesGraphicsLayer
-  //when fired, create a new graphic with the geometry from the event.graphic and add it to the maps graphics layer
-  countyLayer.on("mouse-over", function(evt){
-    var content = evt.graphic.attributes["NAME"] + ": " + evt.graphic.attributes["POP2000"];
-    console.log(content);
-
-    var highlightGraphic = new Graphic(evt.graphic.geometry,highlightSymbol);
-    map.graphics.add(highlightGraphic);
-  });
 
   function removeHighlight() {
+    // this function fires when the mouse leaves a polygon, and removes the highlight from the map
     map.graphics.clear();
   }
 
